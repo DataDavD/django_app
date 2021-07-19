97ff893e37a100e8abfc651973b198b4c6c00b6d
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-07-19 02:13:45 -0700

    Add some simple views for the poll app

--- django_apps/poll/views.py
+++ django_apps/poll/views.py
@@ -4,3 +4,16 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest):
     return HttpResponse("Hello, world. You're at the polls index. Made with love by DataDavD")
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def votes(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % question_id)
