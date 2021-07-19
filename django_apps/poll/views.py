f5fb3f16b8ecd80002b219fd1f3a4e14e111c32e
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-07-19 02:22:58 -0700

    Add poll index showing latest 5 Questions; update project deps

--- django_apps/poll/views.py
+++ django_apps/poll/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 
-def index(request: HttpRequest):
-    return HttpResponse("Hello, world. You're at the polls index. Made with love by DataDavD")
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
