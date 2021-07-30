d3fdb8d19a13aecb5902a0c6e45dcb414f552ea0
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-07-30 00:20:50 -0700

    Create new test on view of QuestionIndexView when there are no questions

--- django_apps/poll/tests.py
+++ django_apps/poll/tests.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.test import TestCase
+from django.urls import reverse
 from django.utils import timezone
 
 from .models import Question
@@ -22,6 +23,26 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_recent_question(self):
         """was_published_recently() returns True for questions whose pub_date is within the last day."""
-        time = timezone.now() + datetime.timedelta(days=23, minutes=59, seconds=59)
+        time = timezone.now() - datetime.timedelta(days=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    """Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """If no questions exist, an appropriate message is displayed."""
+        response = self.client.get(reverse("poll:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
