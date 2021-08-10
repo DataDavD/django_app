f4a804b4f0bccb557b612492250f8fdd6a35a1ed
David Dansby <39511285+DataDavD@users.noreply.github.com>
2021-08-10 01:06:16 -0700

    Add style sheet and green question links

--- django_apps/poll/views.py
+++ django_apps/poll/views.py
@@ -21,7 +21,7 @@ class DetailView(generic.DetailView):
     template_name = "poll/detail.html"
 
     def get_queryset(self):
-        """Excludes any questionsn that aren't published yet."""
+        """Excludes any questions that aren't published yet."""
 
         return Question.objects.filter(pub_date__lte=timezone.now())
 
